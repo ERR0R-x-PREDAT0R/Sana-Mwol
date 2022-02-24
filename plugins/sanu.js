8bbf1e1aa2c0a8c803ad514e1c767b09d9911afb
ALINSHAN <92071951+ERR0R-x-PREDAT0R@users.noreply.github.com>
2022-02-24 11:38:33 +0530

    Update sanu.js

--- plugins/sanu.js
+++ plugins/sanu.js
@@ -26,11 +26,11 @@ Sana.addCommand({pattern: '.git', fromMe: whb, dontAddCommandList: true}, (async
 
     const buttons = [
 
-        {buttonId: 'id1', buttonText: {displayText: '♡︎ ɢɪᴛ ʟɪɴᴋs♡︎ \n\n\nhttps://github.com/Ajx-Abu/Abu_ser\n\n\nhttps://github.com/Ajx-Abu/Sana-Mwol'}, type: 1},
+        {buttonId: 'id1', buttonText: {displayText: '♡︎ ɢɪᴛ ʟɪɴᴋs♡︎ \n\n\nhttps://github.com/ERR0R-x-PREDAT0R/Toxic-Alexa'}, type: 1},
 
-        {buttonId: 'id2', buttonText: {displayText: '♡︎ ɢʀᴏᴜᴘ ʟɪɴᴋ ♡︎\n\n\nhttps://chat.whatsapp.com/Bq0eHs3UpGJ2BKIHOmy7mk'}, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: '♡︎ ɢʀᴏᴜᴘ ʟɪɴᴋ ♡︎\n\n\https://chat.whatsapp.com/EO9KIyXZuWOFED7aPWltCy'}, type: 1},
 
-        {buttonId: 'id3', buttonText: {displayText: '♡︎ ᴏᴡɴᴇʀ ɴᴜᴍʙᴇʀ ♡︎\n\n\nhttp://wa.me/917025994178'}, type: 1}
+        {buttonId: 'id3', buttonText: {displayText: '♡︎ ᴏᴡɴᴇʀ ɴᴜᴍʙᴇʀ ♡︎\n\n\nhttp://wa.me/919383491460'}, type: 1}
 
       ]
 
@@ -40,7 +40,7 @@ Sana.addCommand({pattern: '.git', fromMe: whb, dontAddCommandList: true}, (async
 
           contentText: "♡︎ ᴄʟɪcᴋ ᴛᴏ ʙᴜᴛᴛᴏɴ ♡︎",
 
-          footerText: '™ᴀʙᴜ ©',
+          footerText: '™ᴀʟɪɴsʜᴀɴ©',
 
           buttons: buttons,
 
@@ -50,6 +50,6 @@ Sana.addCommand({pattern: '.git', fromMe: whb, dontAddCommandList: true}, (async
 
       
 
-      await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { mimetype: Mimetype.buttonsMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('./photo/abuser.png'), surface: 200, message: Config.BOT, orderTitle: Config.BOT, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./photo/abuser.png')}}}});
+      await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { mimetype: Mimetype.buttonsMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('./photo/abuser.png'), surface: 200, message: Config.BOT, orderTitle: Config.BOT, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./photo/sana.png')}}}});
 
 }));
